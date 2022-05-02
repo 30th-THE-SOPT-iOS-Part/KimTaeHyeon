976b5099a825df40d2bbdc18c9e88640c05591d6
김태현 <taehyk528@gmail.com>
2022-05-03 01:48:24 +0900

    [Feat] 커스텀 Pop Up

--- CustomView/CustomView/MainScene/MainViewController.swift
+++ CustomView/CustomView/MainScene/MainViewController.swift
@@ -15,14 +15,28 @@ class MainViewController: UIViewController {
 
 extension MainViewController {
     
+    // MARK: - 팝업
     @IBAction func popUpButtonDidTap(_ sender: UIButton) {
         print("팝업")
+        let popUp = PopUp(nibName: "PopUp", bundle: nil)
+        popUp.modalTransitionStyle = .crossDissolve
+        popUp.modalPresentationStyle = .overFullScreen
+        
+        // 애니메이션 없는 ❌
+        present(popUp, animated: true)
+        
+        // 애니메이션 있는 ✅
+//        present(popUp, animated: false) {
+//            popUp.showWithAnimation()
+//        }
     }
     
+    // MARK: - 바텀시트
     @IBAction func bottomSheetButtonDidTap(_ sender: UIButton) {
         print("바텀시트")
     }
     
+    // MARK: - 탭 페이저
     @IBAction func tabPagerButtonDidTap(_ sender: UIButton) {
         print("탭 페이저")
     }
--- CustomView/CustomView/PopUp/PopUp.swift
+++ CustomView/CustomView/PopUp/PopUp.swift
@@ -9,7 +9,43 @@ import UIKit
 
 class PopUp: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var imageView: NSLayoutConstraint!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var joinButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        containerView.layer.cornerRadius = 16
+        joinButton.layer.cornerRadius = 8
+    }
+    
+    @IBAction func closeButtonDidTap(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func joinButtonDidTap(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
+
+extension PopUp {
+    /**
+     - 애니메이션을 곁들인
+     */
+    func showWithAnimation() {
+        self.containerView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+        UIView.animate(withDuration: 0.3) {
+            self.containerView.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.05) {
+                self.containerView.transform = CGAffineTransform.identity
+            }
+        }
     }
 }
